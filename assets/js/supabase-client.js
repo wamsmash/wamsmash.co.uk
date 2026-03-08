be4024a2daa403cf52db610b5b10a7b9f573818c
wamsmash <willedit@proton.me>
2026-03-08 16:44:53 +0000

    Create supabase-client.js

---
+++ assets/js/supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://fkqachwvdeswollxgobd.supabase.co"
+const SUPABASE_ANON_KEY = "sb_publishable_5N68Cgko4Dbt5QKqwDbIEw_4tU_kea"
+
+window.wmSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
