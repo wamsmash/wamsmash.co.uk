a3ec5371e229a0b90502a59b903792996d547185
wamsmash <willedit@proton.me>
2026-03-11 23:03:36 +0000

    Update supabase-client.js

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://fkqachwvdeswollxgobd.supabase.co"
-const SUPABASE_ANON_KEY = "sb_publishable_5N68Cgko4Dbt5QKqwDbIEw_-4tU_kea"
+const SUPABASE_ANON_KEY = "pk_live_51T9P6pCiFsTsh2VQt6WDMWZgwUE7JZKbE7rj074WkSVaImatGjNRcYv7zclAAHIvTBi7yDBkhDMX6MTE5LpnMiEn00MKi6GH2K"
 
 window.wmSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
